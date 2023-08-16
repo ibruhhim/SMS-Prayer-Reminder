03fb19297a78c05664b40ad5811497a14f03b9cf
Ibrahim Ellahi <85767913+IbrahimEllahi@users.noreply.github.com>
2023-08-15 20:44:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -188,7 +188,7 @@ def welcome_users() -> None:
         profile = all_profiles[name]
         phone = profile['phone']
 
-        welcome_message = f"""Hi {first_name}, you're one of the users who've been added to Ibrahim's Islamic Reminders!\nAs we move forward, the introduction of new features may be a possiblity. For now, you can expect to receive prayer reminders based on your profile's location and interesting Islamic Facts. Please note that this project is in its initial stages, and testing will persist as we refine its functionality. Let us collectively strive to expand our knowledge and evolve into the finest versions of ourselves. This application serves as a conduit for us to forge a deeper connection with our Creator and enhance our journey as devout Muslims. It is important to acknowledge that perfection eludes us all, and our shared pursuit is not without its challenges. With this understanding, we embark on a path of continuous improvement, nurturing our spiritual growth and aligning ourselves with the teachings of Islam. Let the purpose of this platform be a source of inspiration, urging us to pursue excellence while recognizing the inherent imperfections that make us human. \nThank you for your time. \n(Report Any Bugs at @ibra.himo on Discord) \nMissed reminders will most likely be a result of downtime and app maintenance."""
+        welcome_message = f"""Hi {first_name}, you're one of the users who've been added to Ibrahim's Islamic Reminders!\n\nAs we move forward, the introduction of new features may be a possiblity. For now, you can expect to receive prayer reminders based on your profile's location and interesting Islamic Facts. Please note that this project is in its initial stages, and testing will persist as we refine its functionality. Let us collectively strive to expand our knowledge and evolve into the finest versions of ourselves. This application serves as a conduit for us to forge a deeper connection with our Creator and enhance our journey as devout Muslims. It is important to acknowledge that perfection eludes us all, and our shared pursuit is not without its challenges. With this understanding, we embark on a path of continuous improvement, nurturing our spiritual growth and aligning ourselves with the teachings of Islam. Let the purpose of this platform be a source of inspiration, urging us to pursue excellence while recognizing the inherent imperfections that make us human. \nThank you for your time. \n(Report Any Bugs at @ibra.himo on Discord) \nMissed reminders will most likely be a result of downtime and app maintenance."""
         twilio.sms(phone=phone, content=welcome_message)
 
 
@@ -206,8 +206,6 @@ async def main() -> None:
 
     await asyncio.gather(*tasks)
 
-me = profiles.search("Ibrahim Ellahi")
-twilio.sms(phone=me['phone'], content="App Deployed")
 
 if __name__ == "__main__":
     asyncio.run(main())
